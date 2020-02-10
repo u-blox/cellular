5b5813ae21f41834daede8b1524e540506c528b1
Rob Meades <rob.meades@u-blox.com>
2020-02-10 20:14:35 +0000

    Switch to Apache 2.0 license header.

--- port/api/cellular_port_clib.h
+++ port/api/cellular_port_clib.h
@@ -1,12 +1,17 @@
 /*
- * Copyright (C) u-blox Melbourn Ltd
- * u-blox Melbourn Ltd, Melbourn, UK
+ * Copyright 2020 u-blox Cambourne Ltd
  *
- * All rights reserved.
- *
- * This source file is the sole property of u-blox Melbourn Ltd.
- * Reproduction or utilisation of this source in whole or part is
- * forbidden without the written consent of u-blox Melbourn Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef _CELLLAR_PORT_CLIB_H_
--- port/api/cellular_port_uart.h
+++ port/api/cellular_port_uart.h
@@ -1,14 +1,19 @@
 /*
- * Copyright (C) u-blox Cambourne Ltd
- * u-blox Cambourne Ltd, Cambourne, UK
+ * Copyright 2020 u-blox Cambourne Ltd
  *
- * All rights reserved.
- *
- * This source file is the sole property of u-blox Cambourne Ltd.
- * Reproduction or utilisation of this source in whole or part is
- * forbidden without the written consent of u-blox Cambourne Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
+ 
 #ifndef _CELLULAR_PORT_UART_H_
 #define _CELLULAR_PORT_UART_H_
 
