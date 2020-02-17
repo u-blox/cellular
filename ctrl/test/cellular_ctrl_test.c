10b8fa95fd5349ec6548553b4bd17818c71961d8
Rob Meades <rob.meades@u-blox.com>
2020-02-17 16:26:45 +0000

    Add tests for power on/off and aliveness functions.

--- ctrl/test/cellular_ctrl_test.c
+++ ctrl/test/cellular_ctrl_test.c
@@ -34,6 +34,9 @@
  * COMPILE-TIME MACROS
  * -------------------------------------------------------------- */
 
+// The amount of time to allow for cellular power off in milliseconds.
+#define CELLULAR_CTRL_TEST_POWER_OFF_TIME_MS 10000
+
 /* ----------------------------------------------------------------
  * TYPES
  * -------------------------------------------------------------- */
@@ -96,6 +99,131 @@ CELLULAR_PORT_TEST_FUNCTION(void cellularCtrlTestInitialisation(),
     // tasks are actually deleted, required by some
     // operating systems (e.g. freeRTOS)
     cellularPortTaskBlock(100);
+}
+
+/** Test power on/off and aliveness, parameterised with the VInt pin.
+ */
+static void cellularCtrlTestPowerAliveVInt(int32_t pinVint)
+{
+    CellularPortQueueHandle_t queueHandle;
+    bool (*pKeepGoingCallback) (void) = NULL;
+
+    CELLULAR_PORT_TEST_ASSERT(cellularPortInit() == 0);
+
+    if (pinVint >= 0) {
+        cellularPortLog("CELLULAR_CTRL_TEST: running power-on and alive tests with VInt on pin %d.\n",
+                        pinVint);
+    } else {
+        cellularPortLog("CELLULAR_CTRL_TEST: running power-on and alive tests without VInt.\n");
+    }
+
+    CELLULAR_PORT_TEST_ASSERT(cellularPortUartInit(CELLULAR_CFG_WHRE_PIN_TXD,
+                                                   CELLULAR_CFG_WHRE_PIN_RXD,
+                                                   CELLULAR_CFG_WHRE_PIN_CTS,
+                                                   CELLULAR_CFG_WHRE_PIN_RTS,
+                                                   CELLULAR_CFG_BAUD_RATE,
+                                                   CELLULAR_CFG_RTS_THRESHOLD,
+                                                   CELLULAR_CFG_UART,
+                                                   &queueHandle) == 0);
+
+    cellularPortLog("CELLULAR_CTRL_TEST: testing power-on and alive calls before initialisation...\n");
+#if (CELLULAR_CFG_WHRE_PIN_ENABLE_POWER) == -1
+    // Should always return true if there isn't a power enable pin
+    CELLULAR_PORT_TEST_ASSERT(cellularCtrlIsPowered());
+#endif
+    // Should return false before initialisation
+    CELLULAR_PORT_TEST_ASSERT(!cellularCtrlIsAlive());
+    // Should fail before initialisation
+    CELLULAR_PORT_TEST_ASSERT(cellularCtrlPowerOn(NULL) < 0);
+    // Should still return false
+    CELLULAR_PORT_TEST_ASSERT(!cellularCtrlIsAlive());
+
+    CELLULAR_PORT_TEST_ASSERT(cellularCtrlInit(CELLULAR_CFG_WHRE_PIN_ENABLE_POWER,
+                                               CELLULAR_CFG_WHRE_PIN_CP_ON,
+                                               pinVint,
+                                               false,
+                                               CELLULAR_CFG_UART,
+                                               queueHandle) == 0);
+
+    // Do this twice so as to check transiting from
+    // a call to cellularCtrlPowerOff() to a call to
+    // cellularCtrlPowerOn().
+    for (size_t x = 0; x < 2; x++) {
+        cellularPortLog("CELLULAR_CTRL_TEST: testing power-on and alive calls");
+        if (x > 0) {
+           cellularPortLog(" with a callback passed to cellularCtrlPowerOff() and a %d second power-off timer.\n",
+                           CELLULAR_CTRL_TEST_POWER_OFF_TIME_MS / 1000);
+        } else {
+           cellularPortLog(" with cellularCtrlPowerOff(NULL).\n");
+        }
+        CELLULAR_PORT_TEST_ASSERT(!cellularCtrlIsAlive());
+#if (CELLULAR_CFG_WHRE_PIN_ENABLE_POWER) != -1
+        CELLULAR_PORT_TEST_ASSERT(!cellularCtrlIsPowered());
+#endif
+        CELLULAR_PORT_TEST_ASSERT(cellularCtrlPowerOn(NULL) == 0);
+        CELLULAR_PORT_TEST_ASSERT(cellularCtrlIsAlive());
+        // Test with and without a keep going callback
+        if (x > 0) {
+            pKeepGoingCallback = keepGoingCallback;
+            gStopTimeMS = cellularPortGetTimeMs() + CELLULAR_CTRL_TEST_POWER_OFF_TIME_MS;
+        }
+        cellularCtrlPowerOff(pKeepGoingCallback);
+    }
+
+    // Do this twice so as to check transiting from
+    // a call to cellularCtrlHardOff() to a call to
+    // cellularCtrlPowerOn().
+    for (size_t x = 0; x < 2; x++) {
+        cellularPortLog("CELLULAR_CTRL_TEST: testing power-on and alive calls with cellularCtrlHardPowerOff().\n");
+        CELLULAR_PORT_TEST_ASSERT(!cellularCtrlIsAlive());
+#if (CELLULAR_CFG_WHRE_PIN_ENABLE_POWER) != -1
+        CELLULAR_PORT_TEST_ASSERT(!cellularCtrlIsPowered());
+#endif
+        CELLULAR_PORT_TEST_ASSERT(cellularCtrlPowerOn(NULL) == 0);
+        CELLULAR_PORT_TEST_ASSERT(cellularCtrlIsAlive());
+        cellularCtrlHardPowerOff();
+    }
+
+    cellularPortLog("CELLULAR_CTRL_TEST: testing power-on and alive calls after hard power off.\n");
+    CELLULAR_PORT_TEST_ASSERT(!cellularCtrlIsAlive());
+#if (CELLULAR_CFG_WHRE_PIN_ENABLE_POWER) != -1
+    CELLULAR_PORT_TEST_ASSERT(!cellularCtrlIsPowered());
+#endif
+
+    cellularCtrlDeinit();
+
+    cellularPortLog("CELLULAR_CTRL_TEST: testing power-on and alive calls after deinitialisation.\n");
+#if (CELLULAR_CFG_WHRE_PIN_ENABLE_POWER) == -1
+    // Should always return true if there isn't a power enable pin
+    CELLULAR_PORT_TEST_ASSERT(cellularCtrlIsPowered());
+#endif
+    // Should fail after deinitialisation
+    CELLULAR_PORT_TEST_ASSERT(cellularCtrlPowerOn(NULL) < 0);
+    // Should return false after deinitialisation
+    CELLULAR_PORT_TEST_ASSERT(!cellularCtrlIsAlive());
+
+    CELLULAR_PORT_TEST_ASSERT(cellularPortUartDeinit(CELLULAR_CFG_UART) == 0);
+
+    cellularPortDeinit();
+
+    // Allow idle task to run so that any deleted
+    // tasks are actually deleted, required by some
+    // operating systems (e.g. freeRTOS)
+    cellularPortTaskBlock(100);
+}
+
+/** Test power on/off and aliveness.
+ */
+CELLULAR_PORT_TEST_FUNCTION(void cellularCtrlTestPowerAlive(),
+                            "powerAndAliveness",
+                            "ctrl")
+{
+
+    // Should work with and without a VInt pin connected
+    cellularCtrlTestPowerAliveVInt(-1);
+#if (CELLULAR_CFG_WHRE_PIN_VINT) != -1
+    cellularCtrlTestPowerAliveVInt(CELLULAR_CFG_WHRE_PIN_VINT);
+#endif
 }
 
 // End of file
